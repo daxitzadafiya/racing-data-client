581b9991313604c9b51a3adc0ed79562db78327a
Daxit Zadafiya <daxitzadafiya.work@gmail.com>
2024-04-17 12:09:44 +0530

    init package

---
+++ src/RandomStringGenerator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RacingPackage;
+
+class RandomStringGenerator
+{
+    public static function generate($length = 10)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return $randomString;
+    }
+}
