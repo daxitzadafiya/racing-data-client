2da70a5414a7dae16f74ee2da51b037fefb7d6ec
Daxit Zadafiya <daxitzadafiya.work@gmail.com>
2024-04-23 11:09:03 +0530

    added .env file

--- src/app/RacingAPIStrategy.php
+++ src/app/RacingAPIStrategy.php
@@ -2,13 +2,32 @@
 
 namespace RacingPackage\app;
 
+use Dotenv\Dotenv;
+use RacingPackage\Exceptions\EnvironmentFileNotFoundException;
+
 class RacingAPIStrategy implements CredentialStrategy
 {
     protected $config;
+    protected $basePath;
 
     public function __construct()
     {
-        $this->config = require_once __DIR__ . "/../config/racing.php";
+        $this->basePath = getcwd();
+
+        // If the environment file does not exist, throw exception.
+        if (! file_exists($this->basePath.'/.env')) {
+            throw new EnvironmentFileNotFoundException('Environment file is missing at '.$this->basePath.'/.env');
+        }
+
+        if (is_file($this->basePath.'/.env')) {
+            // If so, create a new immutable Dotenv instance.
+            $dotenv = Dotenv::createImmutable($this->basePath);
+
+            // Load the environment config.
+            $dotenv->load($this->basePath);
+        }
+
+        $this->config = require_once __DIR__ . "/../../config/racing.php";
     }
 
     public function setCredentials(): array
